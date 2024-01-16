4c5957c344efc2c28e1bcbd060b2b9835c652099
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-01-16 17:06:13 +0200

    created a orderCard component with a container div with illustration image

---
+++ src/Components/OrderCard.js
@@ -0,0 +1,16 @@
+
+
+
+const OrderCard = () => {
+    return (
+        <div className="card-container md:container md:mx-auto object-position: center;">
+        <div classname="illustration ">
+            <img src="./images/illustration-hero.svg" alt="Order illustration"/>
+        </div>
+
+        </div>
+    )
+    
+}
+
+export default OrderCard;
