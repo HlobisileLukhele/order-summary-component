e75cda944d8c681a4287b694690d3c41dd188a0c
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-01-18 00:01:35 +0200

    corrected the path for the illustration path

--- src/Components/OrderCard.js
+++ src/Components/OrderCard.js
@@ -5,7 +5,7 @@ const OrderCard = () => {
     return (
         <div className="card-container md:container md:mx-auto object-position: center;">
         <div classname="illustration ">
-            <img src="./images/illustration-hero.svg" alt="Order illustration"/>
+            <img src="../illustration-hero.svg" alt="Order illustration"/>
         </div>
 
         </div>
