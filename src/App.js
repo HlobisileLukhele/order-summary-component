059872473de686a9a482a31d651b70f928097513
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-01-18 14:43:28 +0200

    imported and renderd displaybackground

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import OrderCard from './Components/OrderCard';
-
+import DisplayBackground from './Components/DisplayBackground';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <DisplayBackground />
       <OrderCard />
           
        </header>
