620c45a8fe5504ca41e66e1ffbbf01bf5915af88
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-01-20 00:14:58 +0200

    removed imported  ordercard

--- src/Components/DisplayBackground.js
+++ src/Components/DisplayBackground.js
@@ -1,9 +1,12 @@
-
+import React from 'react';
+import OrderCard from './OrderCard';
 
 const DisplayBackground = () => {
     return (
         <div className="container-fluid *:display-background">
+        
           <img src="./Assets/images/pattern-background-desktop.svg"  alt="pattern-background-desktop"/>
+          
         </div>
     )
 }
