5678dbd2981bf9ba65567c12796b0c04f85c08d4
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-01-20 00:00:41 +0200

    used boostrap card components

--- src/Components/OrderCard.js
+++ src/Components/OrderCard.js
@@ -3,25 +3,23 @@
 const OrderCard = () => {
     return (
         <div className="container-card md:container md:mx-auto object-position: center;">
-        <div classname="illustration ">
-            <img src="./Assets/images/illustration-hero.svg" alt="Order illustration"/>
-            <h2 className="heading">Order Summary </h2>
-            <p className="text">You can now listen to millions of songs, audiobooks and podcast on any devide anywhere you like</p>
-            <div className="plan-section">
-                <div className=" price-plan">
-                <div className="anual-plan">
-                <img src="./Assets/images/icon-music.svg" alt="icon-music" className="icon"/>
-                 <h4 className="anual"> Anual Plan</h4>
-                 <h5 className="change">Change</h5>
-                </div>
-                <small>$19.90 / month</small>
-                </div>
-                <button className="payment mt-2 mb-2">Proceed to Payment</button>
-                <p clasName="cancel-payment mt-2 mb-2"> Cancel Order</p>
-            </div>
+        <div class="card" style={{width: "20rem"}}>
+        <img src="./Assets/images/illustration-hero.svg" alt="Order illustration" class="card-img-top" />
+        <div class="card-body">
+        <h5 class="card-title mt-4 heading">Order Summary</h5>
+        <p class="card-text mt-4">You can now listen to millions of songs, audiobooks and podcast on any devide anywhere you like.</p>
+        <div className="plan-section mb-2 mt-2">
+            <img src="./Assets/images/icon-music.svg" alt="" />
+            <h4>Anual plan</h4>
+            <small>$59,99/year</small>
         </div>
+        <div>
+        <button class="btn btn-primary">Go somewhere</button>
+        <h5>Cancel Order</h5>
         </div>
+    </div>
+</div>
+</div>  
     )
 }
-
 export default OrderCard;
